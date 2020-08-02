8f7676736f517495fb2b6b6824705319f987fe26
Troy Feng <troy.feng@yale.edu>
2020-08-01 23:34:05 -0400

    Implemented full determinant calculation function on JavaScript as well as input processing. Determinant calculator now fully working.

--- matrix-op-applet/matrix-operations.js
+++ matrix-op-applet/matrix-operations.js
@@ -32,7 +32,9 @@ calculateButton.onclick = function() {
 		return;
 	}
 	outputContainer.style.visibility = "visible";
-	if (outputContainer.innerHTML == "DETERMINANT:") outputContainer.innerHTML += "\n"+det();
+	if (outputContainer.innerHTML.substring(0,1) == "D") {
+		outputContainer.innerHTML = "DETERMINANT: <br>" + calculateDeterminant();
+	}
 }
 
 /* Generate an NxN grid of text fields in grid container. Set id of (i,j)'th element to "ij". */
@@ -53,7 +55,8 @@ function generateGrid(n) {
 
 /* Assert that all NxN text fields are populated with integers. */
 function assertMatInput() {
-	var n = widthReader.value;
+	var n = 0;
+	while (document.getElementById(n+""+n)) n++;
 	for (var i = 0; i < n; i++) {
 		for (var j = 0; j < n; j++) {
 			if (isNaN(document.getElementById(i+""+j).value) || document.getElementById(i+""+j).value == "") {
@@ -64,7 +67,45 @@ function assertMatInput() {
 	return true;
 }
 
-/* Given that text fields in grid are all integers, return determinant of matrix in grid. */
-function det() {
-	return "DET";
+/* Given that text fields in grid are all integers, retrieve inputs and place into NxN array, call det(). */
+function calculateDeterminant() {
+	var n = 0;
+	while (document.getElementById(n+""+n)) n++;
+	var arr = [];
+	for (var i = 0; i < n; i++) {
+		var row = [];
+		for (var j = 0; j < n; j++) {
+			row.push(document.getElementById(i+""+j).value);
+		}
+		arr.push(row);
+	}
+	return det(arr);
 }
+
+/* Recursive determinant calculation function. */
+function det(arr) {
+	var n = arr.length;
+	if (n == 1) return arr[0][0];
+	var ans = 0;
+	for (var j = 0; j < n; j++) {
+		if (j%2 == 0) ans += arr[0][j] * det(subArr(arr,j));
+		else ans -= arr[0][j] * det(subArr(arr,j));
+	}
+	return ans;
+}
+
+/* Given NxN array, return (N-1)x(N-1) array corresponding to removing 0th row and j'th column. */
+function subArr(arr,j) {
+	var n = arr.length;
+	var ans = [];
+	for (var r = 1; r < n; r++) {
+		var row = [];
+		for (var c = 0; c < n; c++) {
+			if (c == j) continue;
+			row.push(arr[r][c]);
+		}
+		ans.push(row);
+	}
+	return ans;
+}
+
