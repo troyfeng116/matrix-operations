ee8e184ee29eb20bb8e7c2ab97fcbb6911efe0bc
Troy Feng <troy.feng@yale.edu>
2020-08-04 15:30:48 -0400

    Added output, second input, and grid containers to html. Changed initial input reader to take different height and width, M and N

--- matrix-op-applet/rref.js
+++ matrix-op-applet/rref.js
@@ -10,7 +10,31 @@ submitButton.onclick = function() {
 		alert("N must be between 1 and 8");
 		return;
 	}
-	widthInputContainer.style.left = "10px";
-	widthInputContainer.style.top = "30%"
-	widthInputContainer.style.transform = "translate(0,-50%)";
+	M = heightReader.value;
+	if (isNaN(M) || M < 1 || M > 8 || M%1 != 0) {
+		alert("M must be between 1 and 8");
+		return;
+	}
+	inputContainer.style.top = "40%";
+	inputContainer.style.left = "10px";
+	inputContainer.style.transform = "translate(0,-50%)";
+	generateGrids();
+	outputContainer.style.visibility = "hidden";
+	secondInputContainer.style.visibility = "visible";
+}
+
+/* Generate an NxN grid of text fields in grid container. Set id of (i,j)'th element to "ij". */
+function generateGrid(n) {
+	while (grid.firstChild) {
+		grid.removeChild(grid.firstChild);
+	}
+	for (var i = 0; i < n; i++) {
+		for (var j = 0; j < n; j++) {
+			var inputField = document.createElement("input");
+			inputField.type="text";
+			inputField.size=3;
+			inputField.id=i+""+j;
+			grid.appendChild(inputField);
+		}
+	}
 }
