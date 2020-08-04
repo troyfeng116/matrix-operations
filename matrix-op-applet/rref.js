ba686ec8aedd545b505559046ec1db3802db8827
Troy Feng <troy.feng@yale.edu>
2020-08-04 15:46:25 -0400

    Implemented submit button event handling, grid generation, and grid input parsing

--- matrix-op-applet/rref.js
+++ matrix-op-applet/rref.js
@@ -1,8 +1,17 @@
-var widthInputContainer = document.getElementById("widthInputContainer");
+var inputContainer = document.getElementById("widthInputContainer");
 var widthReader = document.getElementById("widthReader");
+var heightReader = document.getElementById("heightReader");
 var submitButton = document.getElementById("submitButton");
 
+var grid = document.getElementById("gridContainer");
+
+var secondInputContainer = document.getElementById("secondInputContainer");
+var calculateButton = document.getElementById("calculateButton");
+
+var outputContainer = document.getElementById("outputContainer");
+
 var N;
+var M;
 
 submitButton.onclick = function() {
 	N = widthReader.value;
@@ -18,18 +27,30 @@ submitButton.onclick = function() {
 	inputContainer.style.top = "40%";
 	inputContainer.style.left = "10px";
 	inputContainer.style.transform = "translate(0,-50%)";
-	generateGrids();
+	generateGrid();
 	outputContainer.style.visibility = "hidden";
 	secondInputContainer.style.visibility = "visible";
 }
 
+calculateButton.onclick = function() {
+	if (!assertInputs()) {
+		alert("Make sure all entries are filled with numbers");
+		return;
+	}
+	outputContainer.style.visibility = "visible";
+}
+
 /* Generate an NxN grid of text fields in grid container. Set id of (i,j)'th element to "ij". */
-function generateGrid(n) {
+function generateGrid() {
 	while (grid.firstChild) {
 		grid.removeChild(grid.firstChild);
 	}
-	for (var i = 0; i < n; i++) {
-		for (var j = 0; j < n; j++) {
+	grid.style.height=(36*M)+"px";
+	grid.style.width=(48*N)+"px";
+	grid.style.gridTemplateRows = "repeat("+M+",1fr)";
+	grid.style.gridTemplateColumns = "repeat("+N+",1fr)";
+	for (var i = 0; i < M; i++) {
+		for (var j = 0; j < N; j++) {
 			var inputField = document.createElement("input");
 			inputField.type="text";
 			inputField.size=3;
@@ -37,4 +58,17 @@ function generateGrid(n) {
 			grid.appendChild(inputField);
 		}
 	}
+	grid.style.visibility = "visible";
 }
+
+/* Check if all inputs are filled and numerical. */
+function assertInputs() {
+	for (var i = 0; i < M; i++) {
+		for (var j = 0; j < N; j++) {
+			var x = document.getElementById(i+""+j).value;
+			if (isNaN(x) || x=="") return false;
+		}
+	}
+	return true;
+}
+
