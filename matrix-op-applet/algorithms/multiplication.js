ce3f1eae893b4ea9c58d8e6a6937417c14ffa4b8
Troy Feng <troy.feng@yale.edu>
2020-11-26 11:04:31 -0500

    Clean up multiplication.js, format multiplication.css

--- matrix-op-applet/algorithms/multiplication.js
+++ matrix-op-applet/algorithms/multiplication.js
@@ -1,22 +1,22 @@
-var inputContainer = document.getElementById("widthInputContainer");
-var mReader = document.getElementById("mReader");
-var nReader = document.getElementById("nReader");
-var kReader = document.getElementById("kReader");
-var submitButton = document.getElementById("submitButton");
+const inputContainer = document.getElementById("widthInputContainer");
+const mReader = document.getElementById("mReader");
+const nReader = document.getElementById("nReader");
+const kReader = document.getElementById("kReader");
+const submitButton = document.getElementById("submitButton");
 
-var grid1 = document.getElementById("gridContainer1");
-var grid2 = document.getElementById("gridContainer2");
+const grid1 = document.getElementById("gridContainer1");
+const grid2 = document.getElementById("gridContainer2");
 
-var secondInputContainer = document.getElementById("secondInputContainer");
-var fillZeroButton = document.getElementById("fillZeroButton");
-var calculateButton = document.getElementById("calculateButton");
+const secondInputContainer = document.getElementById("secondInputContainer");
+const fillZeroButton = document.getElementById("fillZeroButton");
+const calculateButton = document.getElementById("calculateButton");
 
-var outputContainer = document.getElementById("outputContainer");
-var outputGrid = document.getElementById("outputGrid");
+const outputContainer = document.getElementById("outputContainer");
+const outputGrid = document.getElementById("outputGrid");
 
-var M;
-var N;
-var K;
+let M;
+let N;
+let K;
 
 submitButton.onclick = function() {
 	M = mReader.value;
@@ -45,17 +45,17 @@ submitButton.onclick = function() {
 }
 
 fillZeroButton.onclick = function() {
-	for (var i = 0; i < M; i++) {
-		for (var j = 0; j < N; j++) {
-			if (document.getElementById(i+""+j+"MN").value == "") {
-				document.getElementById(i+""+j+"MN").value = "0";
+	for (let i = 0; i < M; i++) {
+		for (let j = 0; j < N; j++) {
+			if (document.getElementById(`${i}${j}MN`).value == "") {
+				document.getElementById(`${i}${j}MN`).value = "0";
 			}
 		}
 	}
-	for (var i = 0; i < N; i++) {
-		for (var j = 0; j < K; j++) {
-			if (document.getElementById(i+""+j+"NK").value == "") {
-				document.getElementById(i+""+j+"NK").value = "0";
+	for (let i = 0; i < N; i++) {
+		for (let j = 0; j < K; j++) {
+			if (document.getElementById(`${i}${j}NK`).value == "") {
+				document.getElementById(`${i}${j}NK`).value = "0";
 			}
 		}
 	}
@@ -69,21 +69,21 @@ calculateButton.onclick = function() {
 	while (outputGrid.firstChild) {
 		outputGrid.removeChild(outputGrid.firstChild);
 	}
-	outputGrid.style.width = (60*K)+"px";
-	outputGrid.style.height = (36*M)+"px";
-	outputGrid.style.gridTemplateRows = "repeat("+M+",1fr)";
-	outputGrid.style.gridTemplateColumns = "repeat("+K+",1fr)";
-	for (var i = 0; i < M; i++) {
-		for (var j = 0; j < K; j++) {
-			var entry = document.createElement("div");
-			entry.id=i+""+j;
+	outputGrid.style.width = `${60 * K}px`;
+	outputGrid.style.height = `${36 * M}px`;
+	outputGrid.style.gridTemplateRows = `repeat(${M},1fr)`;
+	outputGrid.style.gridTemplateColumns = `repeat(${K},1fr)`;
+	for (let i = 0; i < M; i++) {
+		for (let j = 0; j < K; j++) {
+			const entry = document.createElement("div");
+			entry.id = `${i}${j}`;
 			outputGrid.appendChild(entry);
 		}
 	}
-	var ans = getProduct();
-	for (var i = 0; i < M; i++) {
-		for (var j = 0; j < K; j++) {
-			document.getElementById(i+""+j).innerHTML = ans[i][j];
+	let ans = getProduct();
+	for (let i = 0; i < M; i++) {
+		for (let j = 0; j < K; j++) {
+			document.getElementById(`${i}${j}`).innerHTML = ans[i][j];
 		}
 	}
 	outputContainer.style.visibility = "visible";
@@ -97,69 +97,69 @@ function generateGrids() {
 		grid2.removeChild(grid2.firstChild);
 	}
 	grid1.style.visibility = "visible";
-	grid1.style.height = (N<=4 && M<=4) ? (48*M)+"px" : (36*M)+"px";
-	grid1.style.width = (N<=4 && M<=4) ? (60*N)+"px" : (48*N)+"px";
-	grid1.style.gridTemplateRows = "repeat("+M+",1fr)";
-	grid1.style.gridTemplateColumns = "repeat("+N+",1fr)";
+	grid1.style.height = N <= 4 && M <= 4 ? `${48 * M}px` : `${36 * M}px`;
+	grid1.style.width = N <= 4 && M <= 4 ? `${60 * N}px` : `${48 * N}px`;
+	grid1.style.gridTemplateRows = `repeat(${M},1fr)`;
+	grid1.style.gridTemplateColumns = `repeat(${N},1fr)`;
 	for (var i = 0; i < M; i++) {
 		for (var j = 0; j < N; j++) {
-			var inputBox = document.createElement("input");
+			const inputBox = document.createElement("input");
 			inputBox.type = "text";
 			inputBox.size = "3";
-			inputBox.id=i+""+j+"MN";
+			inputBox.id = `${i}${j}MN`;
 			grid1.appendChild(inputBox);
 		}
 	}
 	grid2.style.visibility = "visible";
-	grid2.style.width = (N<=4 && K<=4) ? (60*K)+"px" : (48*K)+"px";
-	grid2.style.height = (N<=4 && K<=4) ? (48*N)+"px" : (36*N)+"px";
-	grid2.style.gridTemplateRows = "repeat("+N+",1fr)";
-	grid2.style.gridTemplateColumns = "repeat("+K+",1fr)";
-	for (var i = 0; i < N; i++) {
-		for (var j = 0; j < K; j++) {
-			var inputBox = document.createElement("input");
+	grid2.style.width = N <= 4 && K <= 4 ? `${60 * K}px` : `${48 * K}px`;
+	grid2.style.height = N <= 4 && K <= 4 ? `${48 * N}px` : `${36 * N}px`;
+	grid2.style.gridTemplateRows = `repeat(${N},1fr)`;
+	grid2.style.gridTemplateColumns = `repeat(${K},1fr)`;
+	for (let i = 0; i < N; i++) {
+		for (let j = 0; j < K; j++) {
+			const inputBox = document.createElement("input");
 			inputBox.type = "text";
 			inputBox.size = "3";
-			inputBox.id=i+""+j+"NK";
+			inputBox.id = `${i}${j}NK`;
 			grid2.appendChild(inputBox);
 		}
 	}
 }
 
 function assertInputs() {
-	var allGood = true;
-	for (var i = 0; i < M; i++) {
-		for (var j = 0; j < N; j++) {
-			var x = document.getElementById(i+""+j+"MN").value;
+	let allGood = true;
+	for (let i = 0; i < M; i++) {
+		for (let j = 0; j < N; j++) {
+			const x = document.getElementById(`${i}${j}MN`).value;
 			if (isNaN(x) || x == "") {
 				allGood = false;
-				document.getElementById(i+""+j+"MN").className="invalid";
+				document.getElementById(`${i}${j}MN`).className = "invalid";
 			}
-			else document.getElementById(i+""+j+"MN").className="";
+			else document.getElementById(`${i}${j}MN`).className = "";
 		}
 	}
-	for (var i = 0; i < N; i++) {
-		for (var j = 0; j < K; j++) {
-			var y = document.getElementById(i+""+j+"NK").value;
+	for (let i = 0; i < N; i++) {
+		for (let j = 0; j < K; j++) {
+			const y = document.getElementById(`${i}${j}NK`).value;
 			if (isNaN(y) || y == "") {
 				allGood = false;
-				document.getElementById(i+""+j+"NK").className="invalid";
+				document.getElementById(`${i}${j}NK`).className = "invalid";
 			}
-			else document.getElementById(i+""+j+"NK").className="";
+			else document.getElementById(`${i}${j}NK`).className = "";
 		}
 	}
 	return allGood;
 }
 
 function getProduct() {
-	var ans = [];
-	for (var i = 0; i < M; i++) {
-		var row = [];
-		for (var j = 0; j < K; j++) {
-			var sum = 0;
-			for (var z = 0; z < N; z++) {
-				var x = document.getElementById(i+""+z+"MN").value;
-				var y = document.getElementById(z+""+j+"NK").value;
+	let ans = [];
+	for (let i = 0; i < M; i++) {
+		let row = [];
+		for (let j = 0; j < K; j++) {
+			let sum = 0;
+			for (let z = 0; z < N; z++) {
+				let x = document.getElementById(`${i}${z}MN`).value;
+				let y = document.getElementById(`${z}${j}NK`).value;
 				sum += x*y;
 			}
 			row.push(sum);
